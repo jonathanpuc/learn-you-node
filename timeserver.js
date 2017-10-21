944b556e8f177888d7424e8fd2d480c5e53f200d
Jonathan Puc <jonpucxyz@Jonathans-MacBook-Pro.local>
2017-10-21 23:31:26 +1100

    add timeserver solution

---
+++ timeserver.js
@@ -0,0 +1,21 @@
+const net = require('net');
+const port = (process.argv[2]);
+
+const date = new Date();
+const year = date.getFullYear();
+const month = date.getMonth() + 1;
+if (month < 10) {month = `0${month}`};
+const day = date.getDate();
+if (day < 10) {day = `0${day}`};
+const hour = date.getHours();
+if (hour < 10) {hour = `0${hour}`};
+const minute = date.getMinutes();
+if (minute < 10) {minute = `0${minute}`};
+
+const data = `${year}-${month}-${day} ${hour}:${minute}\n`;
+
+var server = net.createServer((socket) => {
+    socket.end(data);
+})
+
+server.listen(port);
